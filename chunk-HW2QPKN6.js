af11ac48425610837ded489868ebd2c0daebce29
Ernest Lombardi <ernest-lombardi@idexx.com>
2025-01-24 20:06:15 -0500

    deploy products

---
+++ chunk-HW2QPKN6.js
@@ -0,0 +1,9 @@
+import { K as t, jb as e, za as i } from "./chunk-Z6YZYOPG.js";
+var r = class o {
+  static ɵfac = function (m) {
+    return new (m || o)();
+  };
+  static ɵmod = i({ type: o });
+  static ɵinj = t({ imports: [e] });
+};
+export { r as AppModule };
